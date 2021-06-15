8da2c846f262e5765ee005d96a110a8050a712db
MJ Webb <mw@webbm.xyz>
2021-06-16 10:03:08 +1200

    Random X Position setup at base form, needs working on for mass numbers

--- js/artwork.js
+++ js/artwork.js
@@ -8,11 +8,14 @@ var sceneEl = document.querySelector('a-scene');
 let artWork = {
     add: function() {
         for (var i = 0; i < artWorks.length; i++) {
+            let min = -3.733
+            let max = 3.864
+            let randomXPos = Math.random() * (max - min) + min
             console.log(artWorks[i].id.authorOfArt)
             sceneEl = document.querySelector('a-scene');
             var planeId = document.createElement('a-plane');
             planeId.setAttribute('src', artWorks[i].id.urlOfArt);
-            planeId.setAttribute('position', '0 1.47 -4.4265')
+            planeId.setAttribute('position', randomXPos + ' 1.47 -4.4265')
             sceneEl.appendChild(planeId);
         }
     }
