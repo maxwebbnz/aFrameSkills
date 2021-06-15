a368de72e407d1bf0dcb88317a406992a377d813
MJ Webb <mw@webbm.xyz>
2021-06-16 11:00:02 +1200

    Spacing kind of addressed, info boards with art name added in below, re-worked firebase to work with more than one piece of art work

--- firebase/read.js
+++ firebase/read.js
@@ -15,11 +15,19 @@ let id = 'id'
  *========================================================================**/
 fb.read = function() {
     var galDBRoot = firebase.database().ref('gallery/');
-    galDBRoot.on('value', (snapshot) => {
-        const data = snapshot.val();
-        artWorks.push(data)
-        artWork.add()
-        console.log(snapshot.val())
-    });
+    galDBRoot.once('value').then(
+        (_snapshot) => {
+            _snapshot.forEach(function(childSnapshot) {
+                let artObject = {
+                    "nameOfArt": childSnapshot.child("nameOfArt").val(),
+                    "authorOfArt": childSnapshot.child("authorOfArt").val(),
+                    "url": childSnapshot.child('urlOfArt').val(),
+                    "dateOfUpload": childSnapshot.child('dateOfUpload').val(),
+                    "id": childSnapshot.key,
+                }
+                console.log(artWorks)
+                artWork.add(artObject)
+            })
+        });
 
 }
--- js/artwork.js
+++ js/artwork.js
@@ -13,18 +13,33 @@ let artWork = {
      *?  Pulls data from fb.read and transforms them into pieces of art.
      *@return n/a
      *========================================================================**/
-    add: function() {
-        for (var i = 0; i < artWorks.length; i++) {
-            let min = -3.733
-            let max = 3.864
-            let randomXPos = Math.random() * (max - min) + min
-                // console.log(artWorks[i].id.authorOfArt)
-            sceneEl = document.querySelector('a-scene');
-            var planeId = document.createElement('a-plane');
-            planeId.setAttribute('src', artWorks[i].id.urlOfArt);
-            planeId.setAttribute('position', randomXPos + ' 1.47 -4.4265')
-            sceneEl.appendChild(planeId);
-        }
+    add: function(artObjec) {
+        console.log('hello')
+        let min = -3.733
+        let max = 3.864
+        let randomXPos = Math.random() * (max - min) + min;
+        // console.log(artWorks[i].id.authorOfArt)
+        sceneEl = document.querySelector('a-scene');
+        var planeId = document.createElement('a-plane');
+        planeId.setAttribute('src', artObjec.url);
+        planeId.setAttribute('id', artObjec.id);
+        planeId.setAttribute('position', randomXPos + ' 1.47 -4.4265')
+        sceneEl.appendChild(planeId);
+        // Add in Artwork Info Plane
+        var artWorkInfo = document.createElement('a-plane');
+        artWorkInfo.setAttribute('id', artObjec.id + '-infoPlane');
+        artWorkInfo.setAttribute('position', randomXPos + ' 0.683 -4.4265')
+        artWorkInfo.setAttribute('scale', '1.56 0.35 1')
+        artWorkInfo.setAttribute('material', 'color:  #987676')
+        sceneEl.appendChild(artWorkInfo);
+        // Add in Artwork Info Plane - Art Work nameOfArt
+        var artWorkInfoArtName = document.createElement('a-text');
+        artWorkInfoArtName.setAttribute('id', artObjec.id + '-infoPlane(artworkname)');
+        artWorkInfoArtName.setAttribute('position', randomXPos + ' 0.683 -4.4265')
+        artWorkInfoArtName.setAttribute('scale', '0.610 0.610 1')
+        artWorkInfoArtName.setAttribute('text', 'anchor:  align;  align: center; width:  5;  value:' + artObjec.nameOfArt)
+        sceneEl.appendChild(artWorkInfoArtName);
+
     }
 }
 
